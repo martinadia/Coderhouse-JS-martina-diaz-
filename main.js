8b17cf18ff20372ad8aaa9ac260130d0c9119e99
Martina Diaz <marto@MacBook-Air-de-Martina.local>
2025-06-22 23:45:53 -0300

    segunda entrega finalizada

--- main.js
+++ main.js
@@ -1,83 +1,149 @@
-const productos = []
-let bandera = true
+// formulario inicio de sesion 
+const iniciarsesion = document.getElementById('abrirformulario');
+const formulario = document.getElementById('formulario');
+const formularioVisible = localStorage.getItem('formulariovisible');
 
-function creadordeproductos (nombredelproducto, preciodelproducto){
-    const agregadodeproductos = {
-        nombre: nombredelproducto,
-        precio: preciodelproducto,
-    }
-
-    productos.push(agregadodeproductos)
+if (formularioVisible === 'true') {
+    formulario.classList.remove('oculto');
+} else {
+    formulario.classList.add('oculto');
 }
 
-function carritodeproductos() {
-    if(productos.length == 0){
-        alert("No se encuentran productos dentro del carrito")
-        return
-    }
-    let mensaje = 'Los productos dentro del carrito son:\n'
+iniciarsesion.addEventListener('click', () => {
+    formulario.classList.toggle('oculto');
 
-    for (let i = 0; i < productos.length; i++) {
-       mensaje += `\n ${productos[i].nombre} - $ ${productos[i].precio}`
-    }
-
-    alert(mensaje)
-}
-
-function pago(){
-    let mensaje = 'Su carrito contiene los siguientes productos:\n'
-    let total = 0
-    for (let i = 0; i < productos.length; i++) {
-        let precio = Number(productos[i].precio)
-        if (precio <= 0 || isNaN(precio)){
-            mensaje += `\n ${productos[i].nombre} - Precio Invalido`
-        } else {
-            mensaje += `\n ${productos[i].nombre} - $ ${precio}`
-            total += precio
-        }
-    }
-
-    mensaje += `\nTotal a pagar: $ ${total}`
-
-    alert(mensaje)
-}
-
-const menu = 'Bienvenido a "BeautIA, The Best Intelligent Tecnology"\n 1- Iniciar Sesion\n 2- Agregar Producto\n 3- Ver el Carrito\n 4- Continuar con el pago\n 5- Salir'
-
-while(bandera) {
-    let opciones = Number(prompt(menu))
-
-    switch (opciones) {
-        case 1:
-            let username = prompt("Indique su usuario")
-            let contraseña = prompt("Indique su contraseña")
-            break
-        case 2:
-            let nombreaux = prompt("Ingrese el nombre del producto que desea agregar al carrito")
-            let precioaux = prompt("Ingrese el precio del producto que desea agregar al carrito")
-            if (precioaux <= 0){
-                alert("El precio ingresado no es valido, agregue un precio real!")
-            } 
-            creadordeproductos(nombreaux, precioaux)
-            break
-        case 3: 
-            carritodeproductos()
-            break
-        case 4:
-            pago()
-            break
-        case 5:
-            bandera = false
-            break
-        default:
-            alert("Esa opcion no la tenemos, indique un numero que se encuentre dentro de las opciones!")
-            break
-    }     
-}
-
-const btnAbrir = document.getElementById('abrirFormulario');
-const contenedor = document.getElementById('formularioContainer');
-
-btnAbrir.addEventListener('click', () => {
-    contenedor.classList.toggle('oculto');
+    const estaVisible = !formulario.classList.contains('oculto');
+    localStorage.setItem('formulariovisible', estaVisible);
 });
+
+// carrito
+const botoncarrito = document.getElementById('boton-carrito')
+const carritopanel = document.getElementById('carrito')
+const contenedorproductos = document.getElementById('contenedor-productos')
+const cosasdelcarrito = document.getElementById('cosas-del-carrito')
+const botonterminarcompra = document.getElementById('terminar-la-compra')
+const total = document.getElementById('total')
+
+let Carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
+let carritoabierto = JSON.parse(localStorage.getItem('carrito-abierto')) || false
+
+if (carritoabierto) {
+    carritopanel.classList.add('active')
+} else {
+    carritopanel.classList.remove('active')
+}
+
+botoncarrito.addEventListener('click', () => {
+    if (!carritopanel.classList.contains('active')) {
+        carritoabierto = true
+    } else {
+        carritoabierto = false
+    }
+
+    carritopanel.classList.toggle('active')
+
+    localStorage.setItem('carrito-abierto', JSON.stringify(carritoabierto))
+})
+
+const productos = [
+    {
+      id: 1,
+      nombre: "My Mind Mirror (Circle)",
+      precio: 650000,
+      imagen: 'https://i.pinimg.com/736x/45/ac/33/45ac33f2c89266ac8a5c9c9e2143f7d0.jpg',
+      alt: "Espejo que indica los cuidados de la piel con una forma circular"
+    },
+    {
+      id: 2,
+      nombre: "My Mind Mirror (Square)",
+      precio: 700000,
+      imagen: "https://i.pinimg.com/736x/60/86/14/6086147de1901095db14d70dfd381c10.jpg",
+      alt: "Espejo que indica los cuidados de la piel con una forma cuadrada"
+    },
+    {
+      id: 3,
+      nombre: "My Mind Mirror (Wavy)",
+      precio: 750000,
+      imagen: "https://i.pinimg.com/736x/c0/2d/68/c02d689317ed140ee882466a917fd8ec.jpg",
+      alt: "Espejo que indica los cuidados de la piel con una forma ondulada"
+    }
+];
+
+function run () {
+    mostradordeproductos()
+    creaciondeproductos()
+    eventosagregar()
+}
+
+function agregaralcarrito(producto) {
+    Carrito.push(producto)
+    localStorage.setItem("carrito", JSON.stringify(Carrito))
+}
+
+function calculartotal() {
+    return Carrito.reduce((acc, el) => {
+        return acc += Number(el.precio)
+    }, 0)
+}
+
+function buscarapartirdeid(id) {
+    let producto = productos.find((el) => el.id == id)
+
+    return producto
+}
+
+function eventosagregar() {
+    const botones = document.querySelectorAll('.boton-agregar-al-carrito')
+    const botonesarray = Array.from(botones)
+
+    botonesarray.forEach((el) => {
+        el.addEventListener('click', (e) => {
+            let id = e.target.parentNode.id
+            let producto = buscarapartirdeid(id)
+            agregaralcarrito(producto)
+            mostradordeproductos()
+        })
+    })
+}
+
+function creaciondeproductos() {
+    productos.forEach((el) => {
+        let producto =  
+            `<div class="producto" id=${el.id}>
+                <img src=${el.imagen} alt=${el.alt}>
+                <h3>${el.nombre}</h3>
+                <p>Un espejo inteligente que indica los cuidados que tu piel necesita</p>
+                <span class="precio">$${el.precio}</span>
+                <button class="boton-agregar-al-carrito">
+                    Agregar al carrito
+                </button>
+            </div>`
+
+        contenedorproductos.innerHTML += producto
+    })
+}
+
+function mostradordeproductos() {
+    cosasdelcarrito.innerHTML = ''
+    Carrito.forEach((el) => {
+        let producto = 
+            `<div class="producto">
+                <img src=${el.imagen} alt=${el.alt}>
+                <h3>${el.nombre}</h3>
+                <span class="precio">$${el.precio}</span>
+            </div>`
+        cosasdelcarrito.innerHTML += producto
+    })
+
+    total.innerHTML = ''
+    total.innerHTML = `Total a pagar: $${calculartotal()}`
+}
+
+botonterminarcompra.addEventListener('click', () => {
+    Carrito = []
+    localStorage.removeItem("carrito")
+    mostradordeproductos()
+})
+
+run()
